e2de2944d5a8a1eadc13b2604ae944d9903d432b
Warren Parsons <warrenlp@yahoo.com>
2014-01-09 23:58:21 -0800

    first commit

---
+++ src/OpenCV_Rectangle_Detection.cpp
@@ -0,0 +1,23 @@
+//============================================================================
+// Name        : OpenCV_Rectangle_Detection.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+//#include <iostream>
+//using namespace std;
+//
+//#include <opencv2/core/core.hpp>
+//#include <opencv2/imgproc/imgproc.hpp>
+//#include <opencv2/highgui/highgui.hpp>
+//
+//
+//int main() {
+//
+//
+//
+//
+//	return 0;
+//}
